f667188216d4cdf8203e1c8087ab552993487db7
Sydney <sydney.becu@laposte.net>
2014-12-01 15:13:30 +0100

    Ajout des boutons nom/Auteur

--- src/main/java/fr/ensai/projet/client/NomAuteur.java
+++ src/main/java/fr/ensai/projet/client/NomAuteur.java
@@ -1,21 +1,32 @@
 package fr.ensai.projet.client;
 
 import com.google.gwt.core.client.GWT;
+import com.smartgwt.client.util.BooleanCallback;  
+import com.smartgwt.client.util.SC;  
+import com.smartgwt.client.util.ValueCallback;  
+import com.smartgwt.client.widgets.Canvas;  
+import com.smartgwt.client.widgets.Dialog;  
+import com.smartgwt.client.widgets.IButton;  
+import com.smartgwt.client.widgets.Label;  
+import com.smartgwt.client.widgets.events.ClickEvent;  
+import com.smartgwt.client.widgets.events.ClickHandler; 
 import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.util.SC;
+import com.smartgwt.client.widgets.Dialog;
+import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.Label;
-
 import com.smartgwt.client.widgets.layout.HLayout;
 
 public class NomAuteur extends HLayout {
 	private static final int NOMAUTEURHEIGHT=50;
 	private static final int NOMAUTEURWIDTH=500;
-
+	final Label labelAnswer = new Label("Your answer here");
 	//On initialise le LayoutNomAuteur
 	public NomAuteur(){
 		super();
 	  this.setHeight(NOMAUTEURHEIGHT);
 	  //this.setWidth(NOMAUTEURWIDTH);
-	  this.setBackgroundColor("#B40404");
+	  //this.setBackgroundColor("#B40404");
 	  this.setShowEdges(true);
 	  this.setShowResizeBar(false);
 	  
@@ -31,6 +42,47 @@ public class NomAuteur extends HLayout {
 	  //On rattache les labels au Layout Horizontal
 	  this.addMember(nom);
 	  this.addMember(auteur);
-	}
-
+	
+	  
+	  //On rajoute les boutons:
+	 IButton boutonNom = new IButton("Entrer nom");  
+     boutonNom.setLeft(300);  
+     boutonNom.addClickHandler(new ClickHandler() {  
+         public void onClick(ClickEvent event) {  
+             final Dialog dialogProperties = new Dialog();  
+             dialogProperties.setWidth(300);  
+             SC.askforValue("Nouveau nom", "Nom du PCM", "", new ValueCallback() {  
+                   
+                 public void execute(String value) {  
+                     if (value != null) {  
+                         labelAnswer.setContents(value);  
+                     } else {  
+                         labelAnswer.setContents("Cancel");  
+                     }  
+                 }  
+             }, dialogProperties);  
+         }  
+     });  
+     
+     IButton boutonAuteur = new IButton("Entrer Auteur");  
+     boutonAuteur.setLeft(300);  
+     boutonAuteur.addClickHandler(new ClickHandler() {  
+         public void onClick(ClickEvent event) {  
+             final Dialog dialogProperties = new Dialog();  
+             dialogProperties.setWidth(300);  
+             SC.askforValue("Auteur", "Nom de l'auteur", "", new ValueCallback() {  
+                   
+                 public void execute(String value) {  
+                     if (value != null) {  
+                         labelAnswer.setContents(value);  
+                     } else {  
+                         labelAnswer.setContents("Cancel");  
+                     }  
+                 }  
+             }, dialogProperties);  
+         }  
+     });  
+     this.addMember(boutonNom);
+     this.addMember(boutonAuteur);
+}
 }
