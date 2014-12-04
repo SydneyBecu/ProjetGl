392566138d73d4e198782fcc9dab1b2f6c8ff626
Sydney <sydney.becu@laposte.net>
2014-12-04 14:07:28 +0100

    Avancement de l'interface

--- src/main/java/fr/ensai/projet/client/PanneauCommande.java
+++ src/main/java/fr/ensai/projet/client/PanneauCommande.java
@@ -2,9 +2,13 @@ package fr.ensai.projet.client;
 
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.ComboBoxItem;
+import com.smartgwt.client.widgets.form.fields.SelectItem;
 import com.smartgwt.client.widgets.layout.VLayout;
 import com.google.gwt.core.client.GWT;
 import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.types.MultipleAppearance;
 import com.smartgwt.client.types.Overflow;
 import com.smartgwt.client.util.SC;
 import com.smartgwt.client.util.ValueCallback;
@@ -16,7 +20,13 @@ import com.smartgwt.client.widgets.Label;
 public class PanneauCommande extends VLayout {
 	private final static int LARGEUR=300;
 	final Label labelAnswer = new Label("Your answer here");
-	IButton AjoutLigne = new IButton("Entrer nom"); 
+	IButton AjoutLigne = new IButton("Ajouter une ligne"); 
+	IButton AjoutColonne = new IButton("Ajouter une colonne");
+	ComboBoxItem SuppressionLigne = new ComboBoxItem();
+	ComboBoxItem SuppressionColonne = new ComboBoxItem();
+    final SelectItem MasquerLigne = new SelectItem();  
+    final SelectItem MasquerColonne = new SelectItem();  
+
 	public PanneauCommande(){
 		super();
 		this.setWidth(LARGEUR);
@@ -54,7 +64,61 @@ public class PanneauCommande extends VLayout {
         }  
     });  
     
+    AjoutLigne.setLeft(300);  
+    AjoutLigne.addClickHandler(new ClickHandler() {  
+        public void onClick(ClickEvent event) {  
+            final Dialog dialogProperties = new Dialog();  
+            dialogProperties.setWidth(300);  
+            SC.askforValue("Ajout de colonne", "Nom de la colonne", "", new ValueCallback() {  
+                  
+                public void execute(String value) {  
+                    if (value != null) {  
+                        labelAnswer.setContents(value);  
+                    } else {  
+                        labelAnswer.setContents("Cancel");  
+                    }  
+                }  
+            }, dialogProperties);  
+        }  
+    });  
+    
+	
     this.addMember(AjoutLigne);
+    this.addMember(AjoutColonne);
+    
+    //Boutons déroulants pour suppression de ligne et de colonne.
+    
+    DynamicForm formSupp = new DynamicForm();
+    SuppressionLigne.setTitle("Supprimer une ligne");  
+    SuppressionLigne.setType("comboBox");  
+    SuppressionLigne.setValueMap("Yolo", "Swag");  
+
+    
+     
+    SuppressionColonne.setTitle("Supprimer une colonne");  
+    SuppressionColonne.setType("comboBox");  
+    SuppressionColonne.setValueMap("Cat", "Dog");  
+    formSupp.setFields(SuppressionLigne,SuppressionColonne);
+
+    this.addMember(formSupp);
+    
+    
+   // Multi select pour Masquer
+    
+    DynamicForm formMasq = new DynamicForm();
+
+    MasquerLigne.setTitle("Masquer ligne");  
+    MasquerLigne.setMultiple(true);  
+    MasquerLigne.setMultipleAppearance(MultipleAppearance.PICKLIST);  
+    MasquerLigne.setValueMap("Cat", "Dog", "Giraffe", "Goat", "Marmoset", "Mouse");  
+
+    MasquerColonne.setTitle("Masquer colonne");  
+    MasquerColonne.setMultiple(true);  
+    MasquerColonne.setMultipleAppearance(MultipleAppearance.PICKLIST);  
+    MasquerColonne.setValueMap("Cat", "Dog", "Giraffe", "Goat", "y", "Mouse");
+    
+    formMasq.setItems(MasquerLigne,MasquerColonne);
+    this.addMember(formMasq);
     
 
 }
